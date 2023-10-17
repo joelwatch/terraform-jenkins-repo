369fd069bf70b134ad6954ce3fc8d4cdcfd5aab9
joel watchueng <watchuengj@yahoo.fr>
2023-10-16 21:23:41 -0400

    update

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-dev-tf-state-bucket"
+    bucket = "my-dev-tf-state-bucke"
     key = "main"
     region = "us-east-2"
     dynamodb_table = "my-dynamodb-table"
